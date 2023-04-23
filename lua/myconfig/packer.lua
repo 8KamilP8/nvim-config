72a805e245983843af9c495b150baeec83dea533
kamil <kamil@soundprocurement.com>
2023-04-23 21:18:36 +0200

    added git integration

--- lua/myconfig/packer.lua
+++ lua/myconfig/packer.lua
@@ -51,6 +51,7 @@ return require('packer').startup(function(use)
     }
     use 'nvim-tree/nvim-web-devicons'
     use 'ThePrimeagen/harpoon'
+    use 'lewis6991/gitsigns.nvim'
 
     use {
       'nvim-lualine/lualine.nvim',
@@ -60,6 +61,7 @@ return require('packer').startup(function(use)
     use 'saadparwaiz1/cmp_luasnip'
     use 'L3MON4D3/LuaSnip'
     use "rafamadriz/friendly-snippets"
-
+    -- git integration
+    use("tpope/vim-fugitive")
 
 end)
